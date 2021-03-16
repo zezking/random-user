23b8a4826847647d4b68726fa093f69b3dd7dc90
zezking <zhaoenze001@gmail.com>
2021-03-16 10:17:14 -0700

    update readme

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 ### Result
 ![demo](./random.gif)
 
-### This is a practice assignment with React Hooks using useState and UseEffect 
+### This is a simple practice with React Hooks using useState and UseEffect 
 
 This program will generate random user profiles with click with async call and await
 
--- src/Application.js
+++ src/Application.js
@@ -3,13 +3,13 @@ import React, { useState, useEffect } from "react";
 export default function Application() {
   const [user, setUser] = useState(null);
   const [count, setCount] = useState(0);
-
+  
+  const response = await fetch("https://randomuser.me/api/");
+  const data = await response.json();
+  const [item] = data.results;
   useEffect(async () => {
-    const response = await fetch("https://randomuser.me/api/");
-    const data = await response.json();
-    const [item] = data.results;
-    setUser(item);
-  }, [count]);
+  setUser(item);
+}, [count]);
 
   return (
     <div>
